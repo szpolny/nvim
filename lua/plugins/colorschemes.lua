a1a7ecea6452358af371d11aeff56113e608a2bf
Szymon <szymonpolny@outlook.com>
2025-07-01 22:04:11 +0200

    theme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -12,7 +12,7 @@ return {
   {
     'projekt0n/github-nvim-theme',
     config = function()
-      vim.cmd.colorscheme 'github_dark_default'
+      -- vim.cmd.colorscheme 'github_dark_default'
     end,
   },
   { 'savq/melange-nvim' },
@@ -23,15 +23,9 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      -- vim.g.gruvbox_material_background = 'hard'
-      -- vim.g.gruvbox_material_foreground = 'mix'
-      -- vim.g.gruvbox_material_ui_contrast = 'hard'
-      -- vim.g.gruvbox_material_float_style = 'bright'
-      -- vim.g.gruvbox_material_statusline_style = 'material'
-      --
-      -- vim.g.gruvbox_material_better_performance = 1
-      --
-      -- vim.cmd.colorscheme 'gruvbox-material'
+      vim.g.gruvbox_material_background = 'medium'
+
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 }
